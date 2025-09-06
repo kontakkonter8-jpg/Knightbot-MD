0ca4a1e421ce1b81f7f9692e8e07c3c0a9fabc92
kontakkonter8-jpg <kontakkonter8@gmail.com>
2025-09-06 18:56:05 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'PaWally Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "PaWally Bot",
+  botOwner: 'KING AROWAY', // Your name
+  ownerNumber: '+6282192384897', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
